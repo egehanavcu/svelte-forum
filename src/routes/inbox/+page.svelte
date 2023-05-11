0073187116de730b1ae16abd9bb3e43f8be57b64
Egehan <egehanavcu@gmail.com>
2023-05-11 15:16:34 +0300

    Added inbox page

---
+++ src/routes/inbox/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+  import TopicDetail from "../../components/Topic/TopicDetail.svelte";
+</script>
+
+<svelte:head>
+  <title>Conversations</title>
+</svelte:head>
+
+<div class="mb-1" />
+
+<TopicDetail
+  topicTitle="Title"
+  creationAuthor="Egehan"
+  creationDate="01/01/2023 00:00"
+  lastActivityDate="01/01/2023 00:01"
+  lastActivityUsername="Egehan2"
+  commentCount={2}
+  isPrivateMessage={true}
+/>
+
+<TopicDetail
+  topicTitle="Title"
+  creationAuthor="Egehan"
+  creationDate="01/01/2023 00:00"
+  lastActivityDate="01/01/2023 00:01"
+  lastActivityUsername="Egehan2"
+  commentCount={2}
+  isPrivateMessage={true}
+/>
+
+<TopicDetail
+  topicTitle="Title"
+  creationAuthor="Egehan"
+  creationDate="01/01/2023 00:00"
+  lastActivityDate="01/01/2023 00:01"
+  lastActivityUsername="Egehan2"
+  commentCount={2}
+  isPrivateMessage={true}
+/>
+
+<TopicDetail
+  topicTitle="Title"
+  creationAuthor="Egehan"
+  creationDate="01/01/2023 00:00"
+  lastActivityDate="01/01/2023 00:01"
+  lastActivityUsername="Egehan2"
+  commentCount={2}
+  isPrivateMessage={true}
+/>
